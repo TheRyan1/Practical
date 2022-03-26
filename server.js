a2d0378af33de8ed3c18824293bbfa1f8bb0c147
Ryan Zeelie <ryan.zeelie@live.com>
2022-03-26 09:26:57 +0800

    FakeDB udpatesd

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// NB!! Install base dependencies first 
+// NB!! Install base dependencies first
 
 // Import all dependencies :
 // const express = require('express');
@@ -8,7 +8,7 @@
 // Create a port for your webserver
 // const PORT = 3001 || process.env.PORT
 
-// Instantiate the web server : 
+// Instantiate the web server :
 // const app = express();
 
 // Make the server listen on your port
@@ -17,11 +17,18 @@
 //	console.log('Server is up and running')
 
 //   })
-
+let fakeDB = [
+  {
+    author: "Ryan",
+    title: "Do the Dishes",
+    description: "Wash the dishes before making dinner tonight",
+    completed: false,
+  },
+];
 // Create a get route to test :  open your browser and go to http://localhost:3001/
 
 // app.get('/',(req,res)=>{
 
 //	res.send('Welcome to the server')
-   
+
 //   })
