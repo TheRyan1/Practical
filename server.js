66e832ed27b94dc3f0dab061c4bac92b895a4c1b
Ryan Zeelie <ryan.zeelie@live.com>
2022-03-26 09:18:20 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// NB!! Install base dependencies first 
+
+// Import all dependencies :
+// const express = require('express');
+
+// const cors = require('cors')
+
+// Create a port for your webserver
+// const PORT = 3001 || process.env.PORT
+
+// Instantiate the web server : 
+// const app = express();
+
+// Make the server listen on your port
+// app.listen(PORT,()=>{
+
+//	console.log('Server is up and running')
+
+//   })
+
+// Create a get route to test :  open your browser and go to http://localhost:3001/
+
+// app.get('/',(req,res)=>{
+
+//	res.send('Welcome to the server')
+   
+//   })
